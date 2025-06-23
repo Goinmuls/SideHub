762ba0b226efbe47ac5ca19f3e227cde6a5cc203
wlsdks <wlsdks12@naver.com>
2025-06-23 20:40:20 +0900

    :sparkles: first init

---
+++ src/main/java/com/goinmuls/sidehub/SidehubApplication.java
@@ -0,0 +1,13 @@
+package com.goinmuls.sidehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goinmuls/sidehub/SidehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goinmuls.sidehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
