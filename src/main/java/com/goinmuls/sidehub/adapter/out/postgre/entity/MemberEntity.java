540d53a0ca3eb53584ad15fa402eb18c12a94851
wlsdks <wlsdks12@naver.com>
2025-07-24 23:26:58 +0900

    :sparkles: 엔티티 equals & hashcode 가이드 추가

--- src/main/java/com/goinmuls/sidehub/adapter/out/postgre/entity/MemberEntity.java
+++ src/main/java/com/goinmuls/sidehub/adapter/out/postgre/entity/MemberEntity.java
@@ -9,6 +9,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access =  AccessLevel.PROTECTED)
 @Getter
@@ -18,5 +20,18 @@ public class MemberEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    // id 만으로 판별하는 equals와 hashCode 메서드 구현
+    // JPA 엔티티는 equals와 hashCode를 구현할 때, 식별자(id)를 기준으로 비교하는 것이 일반적이다.
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof MemberEntity that)) return false;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
 
 }
