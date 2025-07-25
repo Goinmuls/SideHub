9e8fc129b4bac13f6f89229a6e617a9798f421fc
dabin0513 <dabin0513@naver.com>
2025-07-25 20:33:47 +0900

    :truck: 채팅방 멤버 outport adapter 명칭 변경

--- src/main/java/com/goinmuls/sidehub/adapter/out/postgre/ChatRoomMemberPersistenceAdapter.java
+++ src/main/java/com/goinmuls/sidehub/adapter/out/postgre/ChatRoomMemberPersistenceAdapter.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Component
 @RequiredArgsConstructor
-public class ChatRoomMemberMemberAdapter implements ChatRoomMemberOutport {
+public class ChatRoomMemberPersistenceAdapter implements ChatRoomMemberOutport {
     private final ChatRoomMemberJpaRepository chatRoomMemberJpaRepository;
 
     /**
