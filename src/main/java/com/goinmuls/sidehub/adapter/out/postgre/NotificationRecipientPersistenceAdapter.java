c20c73cea74f170499e388ec72a012c60c86b610
dabin0513 <dabin0513@naver.com>
2025-07-26 09:11:13 +0900

    :sparkles: 알림 대상자 port, adapter, service 추가

---
+++ src/main/java/com/goinmuls/sidehub/adapter/out/postgre/NotificationRecipientPersistenceAdapter.java
@@ -0,0 +1,9 @@
+package com.goinmuls.sidehub.adapter.out.postgre;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class NotificationRecipientPersistenceAdapter {
+}
