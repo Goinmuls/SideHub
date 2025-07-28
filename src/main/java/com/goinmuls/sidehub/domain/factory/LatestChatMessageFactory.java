e315da0783d49069e36ee17d2db0bdccc4c89cf8
dabin <ekqls@sweetk.co.kr>
2025-07-28 13:04:58 +0900

    ✨ 최신 메세지 port, adapter, service 추가

--- src/main/java/com/goinmuls/sidehub/domain/factory/LatestChatMessageFactory.java
+++ src/main/java/com/goinmuls/sidehub/domain/factory/LatestChatMessageFactory.java
@@ -1,7 +1,19 @@
 package com.goinmuls.sidehub.domain.factory;
 
+import com.goinmuls.sidehub.adapter.out.mongo.document.LatestChatMessageDocument;
+import com.goinmuls.sidehub.domain.LatestChatMessage;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LatestChatMessageFactory {
+
+    public LatestChatMessage fromDocument(LatestChatMessageDocument latestChatMessageDocument) {
+        return LatestChatMessage.of(
+                latestChatMessageDocument.getId(),
+                latestChatMessageDocument.getChatRoomId(),
+                latestChatMessageDocument.getMessage(),
+                latestChatMessageDocument.getUnreadMessageCount(),
+                latestChatMessageDocument.getCreatedAt()
+        );
+    }
 }
